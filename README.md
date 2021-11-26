4348386d981138da9589b567caab29d614cad499
vladislava.p <vladyslava.prykhodko@gmail.com>
2021-11-26 19:39:39 +0200

    image support

---
+++ README.md
@@ -0,0 +1,20 @@
+# Images list flipper
+> A simple javascript library for flippering effect of list images on upload.
+## Table of contents
+* [Use](#use)
+* [Features](#features)
+* [Main](#main)
+* [Getting started](#getting-started)
+* [Methods](#methods)
+* [Events](#events)
+* [Visual example](#visual_example)
+* [Browser support](#browser-support)
+* [Support](#support)
+* [License](#license)
+## Use
+This library doesnt use external libraries.
+## Main
+```
+dist/
+├── image-list-flipper.js    ( 49 KB)
+```
--- src/index.js
+++ src/index.js
@@ -7,53 +7,124 @@ function randomInteger(min, max) {
 }
 
 function ImageListFlipper(){
-  this.elements = [];
+  this.count = 0;
   this.backImages = [];
   this.countPreloaded = 0;
   
   document.addEventListener("DOMContentLoaded",()=>{
     const self = this;
-    this.elements = document.querySelectorAll('[image-flipper-back]');
+    this.count = 0;
+    this.elements = [];
+    const parents  = document.querySelectorAll('.image-flipper');
 
-    [].forEach.call((this.elements), function(element, index) {
-      const imageSrc = element.getAttribute('image-flipper-back');
+    this.elements  = document.querySelectorAll('.image-flipper > *');
+    this.count = this.elements.length;
 
-      const style = window.getComputedStyle(element, false)  
-      const bi = style.backgroundImage.slice(4, -1).replace(/"/g, "");  
-      const delay = randomInteger(0, self.elements.length);
-
-      element.setAttribute('image-flipper-id' , index);
+    [].forEach.call((parents), function(parent, index){
+      parent.setAttribute('image-flip-parent-number', index);
+      const gran = parent.parentNode;
+      gran.classList.add("image-flip-gran");
+      const copyParent = document.createElement('div');
+      copyParent.classList.add("image-flip-copy-parent");
+      copyParent.setAttribute('image-flip-parent-number', index);
+      copyParent.innerHTML = gran.innerHTML;
+      gran.insertBefore(copyParent, parent);
       
-      self.backImages.push({
-        backSrc: imageSrc,
-        fromSrc: bi,
-        index: index,
-        element: element,
-        delay: delay * 100,
-      });
+      const elements = parent.children;
+      const copyElements = copyParent.children[0].children;
 
-      self.preload(self, imageSrc);
-      self.preload(self, bi);
+      [].forEach.call((elements), function(element, index) {
+       
+        const imageSrc = element.getAttribute('image-flipper-back');
+  
+        let bi = '';
+        let type = 'bg';
+        if(element.tagName == 'IMG'){
+          bi = element.getAttribute('src');
+          type = 'img';
+        } else {
+          const style = window.getComputedStyle(element, false)  
+          bi = style.backgroundImage.slice(4, -1).replace(/"/g, "");  
+        }
+        
+        const delay = randomInteger(0, self.count);
+  
+        element.setAttribute('image-flipper-id' , index);
+        copyElements[index].setAttribute('image-flipper-copy-id' , index);
+        
+        self.backImages.push({
+          backSrc: imageSrc,
+          fromSrc: bi,
+          index: index,
+          element: element,
+          delay: delay * 100,
+          copyElement: copyElements[index],
+          type: type,
+        });
+  
+        self.preload(self, imageSrc);
+        self.preload(self, bi);
+      });
     });
+
+    window.onresize = function(){
+      self.changeImageWidth(self);
+    }
 
   });
 }
 
+ImageListFlipper.prototype.changeImageWidth = (self) => {
+
+  [].forEach.call((self.backImages), function(item, index) {
+    if(item.type == 'img'){
+      const width = item.copyElement.clientWidth;
+      const height = item.copyElement.clientHeight;
+      console.log(item.element);
+      item.element.style.width = width + 'px';
+      item.element.style.height = height + 'px';
+    }
+  });
+}
+
 ImageListFlipper.prototype.prepare = (self) => {
+  
   [].forEach.call(self.backImages, function(item){
     if(!item.element) return;
-    item.element.style="";
-    item.element.insertAdjacentHTML('afterbegin', '<div class="flip-box"> \
-      <div class="flip-box-inner" style="animation-delay:' + item.delay + 'ms"> \
-        <div class="flip-box-front"> \
-          <div class="flip-item" style="background-image:url(' + item.fromSrc + ')"></div> \
+
+    if(item.type == 'img'){
+      const alt = item.element.getAttribute('alt');
+      const str_img = '<div class="flip-box-inner" style="animation-delay:' + item.delay + 'ms"> \
+          <div class="flip-box-front"> \
+            <img src="' + item.fromSrc + '" alt="' + alt + '"/> \
+          </div> \
+          <div class="flip-box-back"> \
+            <img src="' + item.backSrc + '" alt="' + alt + '"/> \
+          </div> \
+        </div>';
+      const newItem = document.createElement('div');
+      newItem.classList.add("flip-box");
+      newItem.innerHTML = str_img;
+      item.element.parentNode.replaceChild(newItem, item.element);
+      item.element = newItem;
+    } else {
+      const str_bg = '<div class="flip-box"> \
+        <div class="flip-box-inner" style="animation-delay:' + item.delay + 'ms"> \
+          <div class="flip-box-front"> \
+            <div class="flip-item" style="background-image:url(' + item.fromSrc + ')"></div> \
+          </div> \
+          <div class="flip-box-back"> \
+            <div class="flip-item" style="background-image:url(' + item.backSrc + ')"></div> \
+          </div> \
         </div> \
-        <div class="flip-box-back"> \
-          <div class="flip-item" style="background-image:url(' + item.backSrc + ')"></div> \
-        </div> \
-      </div> \
-    </div>');
+      </div>';
+      item.element.style="";
+      item.element.insertAdjacentHTML('afterbegin', str_bg);
+    }
+   
   });
+
+  self.changeImageWidth(self);
 }
 
 ImageListFlipper.prototype.preload = (self, src) => {
@@ -62,7 +133,7 @@ ImageListFlipper.prototype.preload = (self, src) => {
 
   img.onload = function(){
     self.countPreloaded++;
-    if(self.countPreloaded == self.backImages.length * 2){
+    if(self.countPreloaded == self.count * 2){
       self.prepare(self);
     }
   }
